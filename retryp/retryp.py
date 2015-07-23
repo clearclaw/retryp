3cf59825da204a04b0cc6308544700ca1e42c2f1
J C Lawrence <claw@kanga.nu>
2015-07-22 22:56:53 -0700

    #num

--- retryp/retryp.py
+++ retryp/retryp.py
@@ -45,7 +45,7 @@ class retryp (object): # pylint: disable=C0103,R0903
         if self.log_faults:
           logtool.log_fault (e, level = self.log_faults_level)
         else:
-          LOG.info ("Attempt %d failed: %s", attempt, e)
+          LOG.info ("Attempt #%d failed: %s", attempt, e)
         if self.refuse_exc_fn and self.refuse_exc_fn (e):
           LOG.debug ("Exception refused: %s", e)
           raise
