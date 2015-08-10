a1e59a797e92aed744e1fe4ac3c393400fb08556
J C Lawrence <claw@kanga.nu>
2015-08-09 22:49:59 -0700

    New versioning scheme for versioneer

--- setup.py
+++ setup.py
@@ -1,37 +1,28 @@
 #! /usr/bin/env python
 
-try:
-  import pyver
-except ImportError:
-  import pip
-  pip.main (['install', 'pyver'])
-  import pyver # pylint: disable=W0611
-
 from setuptools import setup, find_packages
-
-__version__, __version_info__ = pyver.get_version (pkg = "retryp",
-                                                   public = True)
+import versioneer
 
 setup (
     name = "retryp",
-    version = __version__,
+    version = versioneer.get_version (),
     description = "Well-featured retry decorator",
     long_description = file ("README.rst").read (),
+    cmdclass = versioneer.get_cmdclass (),
     classifiers = [
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: "
-        + "GNU General Public License v3 or later (GPLv3+)",
+        + "GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Topic :: Utilities",
     ],
     keywords = "decorator exception retry retrying",
     author = "J C Lawrence",
     author_email = "claw@kanga.nu",
     url = "https://github.com/clearclaw/retryp",
-    download_url = ("https://github.com/clearclaw/retryp/Tarball/%s.%s"
-                    % (__version_info__[0], __version_info__[1])),
     license = "LGPL v3.0",
     packages = find_packages (exclude = ["tests",]),
+    test_suite = "tests",
     package_data = {
     },
     zip_safe = True,
