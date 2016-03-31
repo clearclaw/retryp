41316abe370500c38a7cf23517d135e7af765b0f
J C Lawrence <claw@kanga.nu>
2016-03-31 16:06:02 -0700

    Log unit

--- retryp/retryp.py
+++ retryp/retryp.py
@@ -47,7 +47,7 @@ class retryp (object): # pylint: disable=C0103,R0903
       except Exception as e:
         if self.log_faults:
           logtool.log_fault (e, level = self.log_faults_level)
-        LOG.info ("Attempt #%d failed: %r  Delay: %d", attempt, e, zzz)
+        LOG.info ("Attempt #%d failed: %r  Delay: %d seconds", attempt, e, zzz)
         if self.refuse_exc_fn and self.refuse_exc_fn (e):
           LOG.debug ("Exception refused: %s", e)
           raise
